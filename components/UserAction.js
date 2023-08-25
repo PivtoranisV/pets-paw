ecd929febbc66edac3008048ee94c444d1f08f30
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-08-26 01:25:02 +0200

    feat(UserAction): implement user logs

--- components/UserAction.js
+++ components/UserAction.js
@@ -1,4 +1,7 @@
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
 import likeIcon from '../public/like-white.svg';
 import dislikeIcon from '../public/dislike-white.svg';
 import favIcon from '../public/fav-white.svg';
@@ -6,52 +9,75 @@ import likeGreen from '../public/like-green.svg';
 import dislikeYellow from '../public/dislike-yellow.svg';
 import favRed from '../public/fav-red.svg';
 
-const DUMMY_LOGS = [
-  {
-    time: '12:12',
-    log: 'Image ID: fQSunHvl8 was added to Favorites',
-    icon: favRed,
-  },
-  {
-    time: '15:15',
-    log: 'Image ID: HJd0XecNX was added to Likes',
-    icon: likeGreen,
-  },
-  {
-    time: '17:12',
-    log: 'Image ID: BbMFS3bU- was added to Dislikes',
-    icon: dislikeYellow,
-  },
-];
+const getCurrentTime = () => {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const time = `${hours}:${minutes}`;
+  return time;
+};
 
 const UserAction = ({ catId }) => {
-  console.log(catId);
+  const [userLogs, setUserLogs] = useState([]);
+
+  const likeHandler = () => {
+    const newLog = {
+      id: catId,
+      time: getCurrentTime(),
+      log: `Image ID: ${catId} was added to Likes`,
+      icon: likeGreen,
+    };
+    setUserLogs([...userLogs, newLog]);
+  };
+
+  const dislikeHandler = () => {
+    const newLog = {
+      id: catId,
+      time: getCurrentTime(),
+      log: `Image ID: ${catId} was added to Dislikes`,
+      icon: dislikeYellow,
+    };
+    setUserLogs([...userLogs, newLog]);
+  };
+  const favHandler = () => {
+    const newLog = {
+      id: catId,
+      time: getCurrentTime(),
+      log: `Image ID: ${catId} was added to Favorites`,
+      icon: favRed,
+    };
+    setUserLogs([...userLogs, newLog]);
+  };
+
   return (
     <div className="flex flex-col mt-[-38px] z-20 relative gap-[14px]">
       <div className="bg-[#282828] flex h-20 gap-1 rounded-[20px] mx-auto">
         <button
           type="button"
           className="bg-[#97EAB9] w-[82px] flex flex-col items-center justify-center rounded-l-[20px]"
+          onClick={likeHandler}
         >
           <Image src={likeIcon} alt="like icon" />
         </button>
         <button
           type="button"
           className="bg-[#FF868E] w-[82px] flex flex-col items-center justify-center"
+          onClick={favHandler}
         >
           <Image src={favIcon} alt="favorite icon" />
         </button>
         <button
           type="button"
           className="bg-[#FFD280] w-[82px] flex flex-col items-center justify-center rounded-r-[20px]"
+          onClick={dislikeHandler}
         >
           <Image src={dislikeIcon} alt="dislike icon" />
         </button>
       </div>
       <div className="flex flex-col gap-[10px]">
-        {DUMMY_LOGS.map((log) => (
+        {userLogs.map((log) => (
           <div
-            key={log.time}
+            key={log.id}
             className="flex bg-[#FFFFFF0D] h-[60px] items-center rounded-[10px] text-primary text-md pl-[15px] justify-between"
           >
             <div className="bg-[#1D1D1D] px-[10px] py-[3px] rounded-[5px] text-white">
